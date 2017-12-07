552b8164d08a0edf94de4d09e822ca7258d0f7f9
Joel Barron <jbarron_web@hotmail.com>
2017-12-06 20:32:21 -0600

    cadena de conexion mongo
    se arreglo la cadena de conexion en mongo db

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 export default {
   port: 3000,
   secret: '19C848F6-33EA-4C7E-A21A-6A06F9157B6A',
-  mongoUrl: 'mongodb://localhost:27017/buzon-quejas'
+  mongoUrl: process.env.MONGODB_URL || 'mongodb://localhost:27017/buzon-quejas'
 }
